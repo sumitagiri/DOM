2e1729f0f9499f50c15fafc39645cdbae94ea0cd
Sumita Giri <sumitagiri594@gmail.com>
2025-08-08 08:38:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//document.querySelector("h1").innerText="subscribe";
+
+const box = document.querySelector('# box');
+box.style.backgroundcolor="yellow";
+box.style.width="100px";
+box.style.height="100px";
+Document.getElementById('box').style.
+backgroundcolor='blue';
+const menuItem=document.querySelectorAll("a")
+//menuItem(0).style.color="red";
+/*menuItem.forEach(function(i){
+    i.style.color='red';
+    i.style.frontsize='24px';
+});*/
+const cusClass=document.getElementsByClassName("nav link");
+const convertedclass=Array.from(cusClass);
+/*for(Leti=0; i<cusClass.length; i++){
+    console.log(cusClass(i));
+}*/ 
+convertedclass.forEach(function(i){
+    i.style.backgroundcolor="#262627";
+    i.style.margin="0 0 15px";
+    i.style.padding="10px 20px"
+    
+});
+document.queryselector('.box').class
+document.querySelector('.box').getAttribute('id');
+document.querySelector('.cta').setAttribute('class','text-white cta');
+
